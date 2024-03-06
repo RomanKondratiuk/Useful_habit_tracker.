639ed7ca12b1fae47d2f9bce889196a3d59684d1
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2024-03-05 23:06:52 -0500

    added 2 models

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import useful_habits
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', include('useful_habits.urls', namespace='useful_habits')),
+    # path('users/', include('users.urls', namespace='users')),
+
 ]
---
+++ useful_habits/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from config import settings
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Habit(models.Model):
+    """ the model for useful habit """
+    action = models.CharField(max_length=255, verbose_name='action')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='user', **NULLABLE)
+
+    def __str__(self):
+        return f"{self.action}"
+
+    class Meta:
+        verbose_name = 'habit'
+        verbose_name_plural = 'habits'
+
+
+class Feeling(models.Model):
+    """ self-awareness"""
+
+    period = [
+        ('week', 'once a week'),
+        ('every_day', 'every day')
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='user', **NULLABLE)
+    place = models.CharField(max_length=255, verbose_name='place of action')
+    action_datetime = models.DateTimeField(verbose_name='data and time of action')
+    action = models.CharField(max_length=255, verbose_name='action')
+    nice_feeling = models.BooleanField(default=False, verbose_name='a sign of a pleasant feeling')
+    related_habit = models.ForeignKey(Habit, on_delete=models.CASCADE, verbose_name='related habit', **NULLABLE)
+    frequency = models.CharField(choices=period, default='every_day', verbose_name="Periodicity")
+    reward = models.CharField(max_length=255, verbose_name='reward', ** NULLABLE)
+    time_to_complete = models.DurationField(verbose_name='time to complete')
+    is_public = models.BooleanField(default=True, verbose_name='sign of publicity')
+
+    def __str__(self):
+        return f"{self.action}"
+
+    class Meta:
+        verbose_name = 'feeling'
+        verbose_name_plural = 'feelings'
+
+
